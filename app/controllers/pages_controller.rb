7de346b54d2f7729f49b65287aa075d4127d3aca
--replace <sodenbyssingle@gmail.com>
2014-02-11 16:52:19 -0800

    Created new homepage and a few subpages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def history
+  end
+
+  def contact
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Pinteresting::Application.routes.draw do
-  get "pages/home"
+  root "pages#home"
+  get "about" => "pages#about" #creates about_path
+  get "contact" => "pages#contact" #creates contact_path
+  get "history" => "pages#History" #creates history_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
