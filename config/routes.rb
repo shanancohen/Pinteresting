c8dd7484088cb2ca556234ffd3db3abb47a7876b
--replace <sodenbyssingle@gmail.com>
2014-02-12 17:59:42 -0800

    includes formating changes to website

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" #creates about_path
   get "contact" => "pages#contact" #creates contact_path
-  get "history" => "pages#History" #creates history_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
